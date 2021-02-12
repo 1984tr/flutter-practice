74b0e435ccb47e93c30eb2cc80236b60125968a9
tris84 <jollytris@gmail.com>
2021-02-12 23:04:11 +0900

    preference practice

--- lib/pages/tab_page02.dart
+++ lib/pages/tab_page02.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TabPage02 extends StatefulWidget {
   @override
@@ -7,17 +8,53 @@ class TabPage02 extends StatefulWidget {
 }
 
 class _TabPage02State extends State<TabPage02> {
+  var test = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadText();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: RaisedButton(
-        child: Text("Show Snackbar"),
-        onPressed: () {
-          // 스낵바 출력 시도 - 정상 출력
-          Scaffold.of(context)
-              .showSnackBar(SnackBar(content: Text("SnackBarTest2")));
-        },
-      ),)
-    );
+        child: Center(
+      child: Column(
+        children: [
+          RaisedButton(
+            child: Text("Show Snackbar"),
+            onPressed: () {
+              // 스낵바 출력 시도 - 정상 출력
+              Scaffold.of(context)
+                  .showSnackBar(SnackBar(content: Text("SnackBarTest2")));
+            },
+          ),
+          RaisedButton(
+            child: Text(test),
+            onPressed: () {
+              _updateText();
+            },
+          )
+        ],
+      ),
+    ));
+  }
+
+  _loadText() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var text = (prefs.getString('test') ?? "unknown");
+    setState(() {
+      test = text;
+    });
+  }
+
+  _updateText() async {
+    var prefs = await SharedPreferences.getInstance();
+    var updated = "Text ${DateTime.now()}";
+    await prefs.setString('test', updated);
+    setState(() {
+      test = updated;
+    });
   }
 }
