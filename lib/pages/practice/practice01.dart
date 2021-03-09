3d145cacea13027c4e381065bcaba12d3a5747b0
tris84 <jollytris@gmail.com>
2021-03-09 23:33:46 +0900

    practice01

--- lib/pages/practice/practice01.dart
+++ lib/pages/practice/practice01.dart
@@ -6,19 +6,56 @@ class Practice01 extends StatefulWidget {
 }
 
 class _Practice01State extends State<Practice01> {
+  var _count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _count = 100;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Practice01"),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            
-          ],
+        appBar: AppBar(
+          title: Text("Practice01"),
         ),
-      ),
-    );
+        body: Container(
+            child: Column(children: [
+          RaisedButton(
+              onPressed: () {
+                setState(() {
+                  _count++;
+                });
+              },
+              child: Text("$_count")),
+          CustomButton(
+            display: Text("display"),
+            onPressed: () => {
+              setState(() {
+                _count++;
+              })
+            },
+          ),
+          Container(color: Colors.lightGreen,
+          child: Row(
+            children: [
+            Text("Col1"),
+            Text("Col2")
+          ],))
+        ])));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final Widget display;
+  final VoidCallback onPressed;
+
+  CustomButton({this.display, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+        child: display, onPressed: onPressed, color: Colors.amber);
   }
 }
