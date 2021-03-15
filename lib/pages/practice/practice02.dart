d4c3c1cb01c82c30a41e706114f16486cb69721a
tris84 <jollytris@gmail.com>
2021-03-15 23:20:10 +0900

    dismissible

--- lib/pages/practice/practice02.dart
+++ lib/pages/practice/practice02.dart
@@ -7,6 +7,10 @@ class Practice02 extends StatefulWidget {
 }
 
 class _Practice02State extends State<Practice02> {
+  var _color = Colors.green;
+  var _text = "Tap Me";
+  var _check = false;
+
   @override
   void initState() {
     super.initState();
@@ -18,15 +22,58 @@ class _Practice02State extends State<Practice02> {
         appBar: PreferredSize(
             preferredSize: Size.fromHeight(ui.appBarHeight(context)),
             child: Container(color: Colors.amber)),
-        body: ListView.builder(
-          shrinkWrap: true,
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Container(
-              color: Colors.green,
-              child: Text("Test$index"),
-            );
-          },
-        ));
+        body: Container(
+            child: Column(children: [
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  _color = Colors.lightGreen;
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  _color = Colors.deepOrange;
+                });
+              },
+              onVerticalDragUpdate: (DragUpdateDetails v) =>
+                  _handleDragEnd(v, context),
+              child: Container(
+                  width: double.infinity,
+                  height: 100,
+                  color: _color,
+                  child: Text(_text))),
+          Dismissible(
+              onDismissed: (DismissDirection dir) => _handleDismiss(dir),
+              confirmDismiss: (DismissDirection dir) async =>
+                  dir == DismissDirection.endToStart,
+              background: Container(
+                child: Icon(Icons.delete_forever),
+                decoration: BoxDecoration(color: Colors.red[700]),
+              ),
+              key: ValueKey("test1"),
+              child: CheckboxListTile(
+                value: _check,
+                title: Text("Title$_check"),
+                onChanged: (b) => _handleCheckbox(b),
+              ))
+        ])));
+  }
+
+  void _handleDragEnd(DragUpdateDetails d, BuildContext context) {
+    double globalPosition = d.globalPosition.dy;
+    double localPosition = d.localPosition.dy;
+    setState(() {
+      _text = "globalPosition: $globalPosition\nlocalPosition: $localPosition";
+    });
+  }
+
+  void _handleDismiss(DismissDirection dir) {
+    print(dir);
+  }
+
+  void _handleCheckbox(bool b) {
+    setState(() {
+      _check = b;
+    });
   }
 }
